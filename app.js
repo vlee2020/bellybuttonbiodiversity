519d193dfd2c16b3889b3cd3a81fcba404cf982a
vlee <findvanessalee@gmail.com>
2021-01-04 18:14:08 -0800

    moved json file out of folder and updated references in html and js files

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ function init() {
   var selector = d3.select("#selDataset");
 
   // Use the list of sample names to populate the select options
-  d3.json("./data/samples.json").then((data) => {
+  d3.json("samples.json").then((data) => {
     var sampleNames = data.names;
     sampleNames.forEach((sample) => {
       selector
